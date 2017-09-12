eae4cfbc6a6dbde3d886cb19e4909d9b932762c5
Lyrichu <919987476@qq.com>
2017-09-12 14:04:23 +0800

    Update kimmyzhang_20170906_03.py

--- 20170906/kimmyzhang_20170906_03.py
+++ 20170906/kimmyzhang_20170906_03.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # 任意输入N个数
 
 List = [1, -1, 4, 3,5, 8]
@@ -13,19 +12,3 @@ for i in range(length - 2):
     Temp.append(temp_distance)
 
 print(min(Temp))
-=======
-# 任意输入N个数
-
-List = [1, -1, 4, 3,5, 8]
-length = len(List)
-summ = 0
-for i in range(length - 1):
-    summ += abs(List[i + 1] - List[i])
-
-Temp = []
-for i in range(length - 2):
-    temp_distance = summ - abs(List[i + 1] - List[i]) - abs(List[i + 2] - List[i + 1 ]) + abs(List[i + 2] - List[i])
-    Temp.append(temp_distance)
-
-print(min(Temp))
->>>>>>> bb930bf1c97f382ceb5b2a0100fcc80aeb382ffb
