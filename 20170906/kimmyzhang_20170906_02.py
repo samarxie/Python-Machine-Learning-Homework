c7f8da9cad6b228c1ad84d8b7562e539b955346d
Lyrichu <919987476@qq.com>
2017-09-12 14:02:40 +0800

    Update kimmyzhang_20170906_02.py

--- 20170906/kimmyzhang_20170906_02.py
+++ 20170906/kimmyzhang_20170906_02.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #接受一个数字
 
 List = [0, 1]
@@ -12,18 +11,3 @@ for i in range(20):
         temp = List[i] + List[i - 1]
         print(temp)
         List.append(temp)
-=======
-#接受一个数字
-
-List = [0, 1]
-
-for i in range(20):
-    if i == 0:
-        print(0)
-    if i == 1:
-        print(1)
-    else:
-        temp = List[i] + List[i - 1]
-        print(temp)
-        List.append(temp)
->>>>>>> bb930bf1c97f382ceb5b2a0100fcc80aeb382ffb
